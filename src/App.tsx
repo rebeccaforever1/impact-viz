1fe91200d397a8df34d2904bc350084d6afa46c3
Rebecca <55362822+rebeccaforever1@users.noreply.github.com>
2025-10-31 13:59:07 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MetroImpactDashboard from './components/MetroImpactDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <MetroImpactDashboard />
+    </div>
+  );
+}
+
+export default App;
